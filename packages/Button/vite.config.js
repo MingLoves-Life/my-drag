f08f79a2ec7b9c416a379296dbe2b743172834a1
wuhaoming1 <wuhaoming1@tal.com>
2022-10-28 18:23:17 +0800

    feat:pnpm

--- packages/Button/vite.config.js
+++ packages/Button/vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        component: "index.html",
+      },
+      output: { dir: "dist", format: "cjs" },
+    },
+  },
+  base: "./",
   plugins: [vue()],
 });
