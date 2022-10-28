e583168121668959c41c1cd0f946da3d7c23c26d
wuhaoming1 <wuhaoming1@tal.com>
2022-10-28 18:32:55 +0800

    feat:pnpm

---
+++ packages/Button/src/component/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <a-button>{{ props.value }}</a-button>
+</template>
+<script setup>
+import { defineComponent } from "vue";
+
+defineComponent({ name: "MyBtn" });
+
+const props = defineProps(["value"]);
+</script>
