58a23395c710a98f573bfc68eba483ea50fe9e17
wuhaoming1 <wuhaoming1@tal.com>
2022-10-31 10:18:31 +0800

    feat:gitigore

--- EditorPage/src/component/MyText.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <a-textarea v-model:value="props.value" placeholder="" :rows="4" :maxlength="6" />
-</template>
-<script setup>
-import { defineComponent } from 'vue';
-defineComponent({ name: 'MyText' });
-
-const props = defineProps(['value'])
-</script>
--- EditorPage/src/store/headerInfo.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useHeaderInfoStore = defineStore("headerInfo", {
-  state: () => ({
-    canvasSize: { width: 600, height: 600 },
-  }),
-
-  actions: {},
-});
